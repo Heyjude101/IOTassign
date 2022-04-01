5164c2ab2f75382318feff81fbe0b0e47ea31a4d
heyjude101 <ritikraj2884@gmail.com>
2022-04-01 19:51:13 +0530

    removed api

--- app/src/main/java/com/example/iotassign/activity/MainActivity.kt
+++ app/src/main/java/com/example/iotassign/activity/MainActivity.kt
@@ -22,8 +22,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var recyclerviewUsers: RecyclerView
-//    val BASE_URL = "https://makeup-api.herokuapp.com/"
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
